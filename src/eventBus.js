6e8bfb30814de98d59c704cbcf872a0152c7d51f
Graceful-Hao <2608848569@qq.com>
2023-05-24 18:55:41 +0800

    update

--- src/eventBus.js
+++ src/eventBus.js
@@ -1,4 +1,4 @@
-// 用对象实现
+// 方法一
 
 const eventBus = {
   // 用来存放 订阅 和 发布的关系的
@@ -12,20 +12,59 @@ const eventBus = {
   },
   // 事件发布
   emit: (type, data) => {
-    const q = callback[type];
-    if (q && q.length > 0) {
-      q.forEach((fn) => fn(data));
+    const fnList = callback[type];
+    if (fnList && fnList.length > 0) {
+      fnList.forEach((fn) => fn(data));
     }
   },
   // 取消订阅
   off: (type, fn) => {
-    const q = callback[type];
-    if (q && q.length > 0) {
-      const index = q.indexOf(fn);
+    const fnList = callback[type];
+    if (fnList && fnList.length > 0) {
+      const index = fnList.indexOf(fn);
       if (index < 0) {
         return;
       }
-      q.splice(index, 1);
+      fnList.splice(index, 1);
     }
   },
 };
+
+/* 
+  eventHub.on("click", console.log);
+  eventHub.on("click", console.error);
+  
+  setTimeout(() => {
+    eventHub.emit("click", "frank");
+  }, 3000); */
+
+// 方法二 用class实现
+
+class EventBus {
+  constructor() {
+    this.callback = {};
+  }
+  // 事件订阅
+  on(type, fn) {
+    this.callback[type] = this.callback[type] || [];
+    this.callback[type].push(fn);
+  }
+  // 事件发布
+  emit(type, data) {
+    const fnList = this.callback[type];
+    if (fnList && fnList.length > 0) {
+      fnList.forEach((fn) => fn(data));
+    }
+  }
+  // 取消订阅
+  off(type, fn) {
+    const fnList = this.callback[type];
+    if (fnList && fnList.length > 0) {
+      const index = fnList.indexOf(fn);
+      if (index < 0) {
+        return;
+      }
+      fnList.splice(index, 1);
+    }
+  }
+}
